baeafbe47330a867204d37cd9725f0ec65f6be48
deepesh00 <deepeshiitb15@gmail.com>
2017-01-20 09:58:52 +0530

    Add files via upload

---
+++ gr.py
@@ -0,0 +1,38 @@
+from sympy import *
+from gravipy import *
+
+
+R, theta , phi = symbols("R, theta, phi")
+x = Coordinates('\chi', [theta, phi])
+
+
+### PRINT METRIC ###
+Metric = diag(R**2, R**2*sin(theta)**2)
+g = MetricTensor('g',x,Metric)
+print "Metric is : " 
+print g(All,All)
+print "\n"
+
+
+### COMPUTE CHRISTOFFEL SYMBOL ###
+Ga = Christoffel('Ga',g)
+#print Ga.components      ----- This command is not working for me
+print "Christoffel symbol is : " 
+print Ga(-All,All,All)
+print "\n"
+
+
+### COMPUTE RIEMANN TENSOR ###
+Rm = Riemann('Rm',g)
+print "Riemann tensor is : "
+print Rm(-All,All,All,All)
+print "\n"
+
+
+### COMPUTE RICCI TENSOR AND RICCI SCALAR ###
+Ri = Ricci('Ri',g)
+print "Ricci tensor is : " 
+print Ri(All, All)
+print "\n"
+print "Ricci scalar is : " , Ri.scalar()
+print "\n"
